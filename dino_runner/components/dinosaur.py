5ddf48e78e2509f8e4b4837f6bcbbb4e6882c2bf
SamuelRicardoo <sricardosgomes@hotmail.com>
2023-06-19 21:00:32 -0300

    Aula 3 - CLASS OBSTACLE

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.sprite import Sprite
+
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
@@ -6,7 +8,8 @@ Y_POS = 310
 JUMP_VEL = 8.5
 Y_POSDUCK = 340
 
-class Dinosaur:
+
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -18,7 +21,6 @@ class Dinosaur:
         self.dino_jump = False
         self.dino_duck = False
         
-    
     def update(self, user_input):
         if self.dino_run:
             self.run()
@@ -31,7 +33,6 @@ class Dinosaur:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
@@ -41,17 +42,14 @@ class Dinosaur:
             self.dino_duck =True
             self.dino_jump = False
             self.dino_run = False
-
         elif not self.dino_jump :
             self.dino_duck = False
             self.dino_jump = False
             self.dino_run = True
 
-
         if self.step_index >= 10:
             self.step_index = 0
-
-    
+ 
     def run (self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
         self.dino_rect = self.image.get_rect()
@@ -70,9 +68,8 @@ class Dinosaur:
                 self.dino_jump = False
                 self.jump_vel = JUMP_VEL 
 
-
     def duck(self):
-        self.image = self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POSDUCK
