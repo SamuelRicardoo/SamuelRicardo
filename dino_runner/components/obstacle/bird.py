94ce6c2f467dc95bf09d163ef223ca05b823f279
SamuelRicardoo <sricardosgomes@hotmail.com>
2023-06-20 20:43:24 -0300

    Aula 4

--- dino_runner/components/obstacle/bird.py
+++ dino_runner/components/obstacle/bird.py
@@ -2,20 +2,19 @@ import random
 
 from dino_runner.components.obstacle.obstacle import Obstacle
 
-Y_POS_BIRD = random.randrange(40, 250, 90)
-
 
 class Bird(Obstacle):
     def __init__(self,image):
         self.type = 0
         super().__init__(image, self.type)
-        self.rect.y = Y_POS_BIRD
+        self.rect.y = self.generate_y_bird()
         self.index = 0
 
+    def generate_y_bird(self):
+        return random.randrange(50,250,90)    
+
     def draw(self, screen):
-        global Y_POS_BIRD
-        if self.index >= 9:
+        if self.index >= 10:
             self.index = 0
         screen.blit(self.image[self.index//5],(self.rect.x, self.rect.y))
         self.index +=1
-        Y_POS_BIRD = random.randrange(40, 250, 90)     
